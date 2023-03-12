9d7c076115dbb0299c372d9cdc53506c7511fc19
Oksana Macko <oksanamacko@gmail.com>
2023-03-12 23:50:43 +0100

    Add discussions#destroy and fix turbo stream

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,30 +2,52 @@
 
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_discussion, only: %i[edit update destroy]
 
   def index
-  	@discussions = Discussion.all
+    @discussions = Discussion.all
   end
 
   def new
-  	@discussion = Discussion.new
+    @discussion = Discussion.new
   end
 
   def create
-  	@discussion = Discussion.new(discussion_params)
+    @discussion = Discussion.new(discussion_params)
 
     respond_to do |format|
-	  	if @discussion.save
-	  		format.html { redirect_to discussions_path, notice: 'Discussion created' }
-	  	else
-	  		format.html { render :new, status: :unprocessable_entity }
-	  	end
-	  end
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: 'Discussion created' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: 'Discussion updated' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @discussion.destroy!
+    redirect_to discussions_path, status: :see_other, notice: 'Discussion deleted'
   end
 
   private
 
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
+  end
+
   def discussion_params
-  	params.require(:discussion).permit(:name, :pinned, :closed)
+    params.require(:discussion).permit(:name, :pinned, :closed)
   end
 end
